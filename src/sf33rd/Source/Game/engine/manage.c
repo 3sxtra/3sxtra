52fa68d27f4760b1bf27596ef410e3d16b09de7e
3sxtra <3sxtra@users.noreply.github.com>
2026-03-29 20:24:15 +0100

    reverted wrong fixes

--- src/sf33rd/Source/Game/engine/manage.c
+++ src/sf33rd/Source/Game/engine/manage.c
@@ -1907,12 +1907,6 @@ static void Judge_Winner() {
             return;
         }
 
-        if (Mode_Type == MODE_NETWORK) {
-            Winner_id = 0;
-            Loser_id = 1;
-            return;
-        }
-
         Winner_id = Champion;
         Loser_id = Champion ^ 1;
         return;
